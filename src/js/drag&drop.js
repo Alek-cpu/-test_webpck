196bf22b6ab997200502d32e2512a931aa7bb982
Aleksandr Pryanikov <a.pryanikov@truemachine.ru>
2020-04-20 18:18:03 +0400

    Рабочая версия drag&drop и удаление

--- src/js/drag&drop.js
+++ src/js/drag&drop.js
@@ -1,17 +1,13 @@
 const dragNDrop = () => {
-    const cardContainer = document.querySelector('.drag-drop-wrapper');
-    const cards = document.querySelectorAll('.event-wrap');
-    const cells = document.querySelectorAll('.card');
-    const basket = document.querySelectorAll('.drag-drop-wrapper > .card > .event-wrap > .identical-inner > .status-event > .edit-control > .deleted');
-
-
+    const cards = document.querySelectorAll('.card');
+    const basket = document.querySelectorAll('.deleted');
 
     function removeCard () {
         for ( let i = 0; i < basket.length; i++ ) {
             for (let j = 0; j < cards.length; j++) {
-                cells[i].addEventListener('click', function (e) {
+                basket[i].addEventListener('click', function (e) {
                     if ( i == j ) {
-                        cards[j].remove();
+                        cards[i].remove();
                     }
                 })
             }
--- src/js/testjsdrop.js
+++ src/js/testjsdrop.js
@@ -1,11 +1,11 @@
 const dDrope = () => {
-    const cards = document.querySelectorAll('.event-wrap');
-    const cells = document.querySelectorAll('.card');
+    const cards = document.querySelectorAll('.card');
+    const cell = document.querySelector('.drag-drop-wrapper');
 
     const dragStart = function () {
         setTimeout(() => {
             this.classList.add('hide');
-        }, 0)
+        }, 0);
     };
 
     const dragEnd = function () {
@@ -13,21 +13,13 @@ const dDrope = () => {
     };
 
     const dragOver = function (e) {
-        console.log('over');
-        let cellWrap;
-        // cellWrap.append();
+        e.preventDefault();
 
-            e.preventDefault();
+        console.log('over');
     };
 
     const dragEnter = function () {
         console.log('enter');
-        // this.classList.add('hovered');
-        //при входе на ячейку наша
-        //карточка, на которую наводим переходит в буфер
-        //ячейка очищается, карточка, которую держим встает в переменную
-        //прошлой карточки, а предыдущая ячейка принимает
-        //значение ушедшей с прежнего места карточки
     };
 
     const dragLeave = function () {
@@ -35,30 +27,20 @@ const dDrope = () => {
         this.classList.remove('hovered');
     };
 
-
-
     const dragDrop = function () {
         console.log('drop');
-        setTimeout(() => {
-            this.classList.remove('hide');
-        }, 0)
-
-        this.append(cards[0]);//ячейки принимает в себя
+        cell.classList.remove('hide');
+        cell.appendChild(this);//ячейки принимает в себя
     };
 
-
-    cells.forEach((cell) => {
-        cell.addEventListener('dragover', dragOver);
-        cell.addEventListener('dragenter', dragEnter);
-        cell.addEventListener('dragleave', dragLeave);
-        cell.addEventListener('drop', dragDrop);
-    });
-
-
+    cell.addEventListener('dragover', dragOver);
+    cell.addEventListener('dragenter', dragEnter);
+    cell.addEventListener('dragleave', dragLeave);
 
     cards.forEach((card) => {
         card.addEventListener('dragstart', dragStart);
         card.addEventListener('dragend', dragEnd);
+        card.addEventListener('drop', dragDrop);
     });
- }
- dDrope();
+}
+dDrope();
